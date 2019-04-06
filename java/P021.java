912d65ae04df6b375539f37b92cff8c9d2924277
markalos <goknowy@gmail.com>
2019-04-06 14:35:44 +0800

    get amicable sum

--- java/P021.java
+++ java/P021.java
@@ -5,13 +5,31 @@ class P021 {
 	public static void main(String[] args) {
 		int res = 0;
 		int limit = 10000;
-		List<Integer> sumArray = IntStream.range(0, limit).map(num -> sumOfProperDivisors(num)).boxed().collect(Collectors.toList());
+		List<Integer> sumArray = getDivisorsSumArray(limit);
 		System.out.println(getAmicableSum(sumArray));
 	}
 
 	static int getAmicableSum(List<Integer> arr) {
-		int res = IntStream.range(0, arr.size()).map(idx -> (arr.get(idx) < arr.size() && arr.get(idx) != idx && arr.get(arr.get(idx)) == idx) ? idx : 0).sum();
+		int res = IntStream.
+				range(0, arr.size()).
+				map(idx ->
+					isAmicalNumber(idx, arr) ?
+				 	idx : 0).
+					sum();
 		return res;
+	}
+
+	static private boolean isAmicalNumber(
+		int idx, 
+		List<Integer> divisorsSumArray
+		) {
+		int pair = divisorsSumArray.get(idx);
+		return pair < divisorsSumArray.size() && pair != idx && divisorsSumArray.get(pair) == idx;
+
+	}
+
+	static private List<Integer> getDivisorsSumArray(int limit) {
+		return IntStream.range(0, limit).map(num -> sumOfProperDivisors(num)).boxed().collect(Collectors.toList());
 	}
 
 	static private int sumOfProperDivisors(int n) {
